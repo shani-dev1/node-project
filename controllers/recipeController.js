3055cd426e8293dd3afcf86b6081ba6ca46ccf93
שני גוטליב <ShaniGo@smg.local>
2025-02-26 19:28:38 +0200

    addRecipe1

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,14 +1,20 @@
 const Recipe = require('../models/Recipe');
 
-
-exports.createRecipe = async (req, res) => {
+exports.addRecipe = async (req, res) => {
+    const reipe = await Recipe.create(req.body)
+    res.json(reipe)
 };
 
 
-exports.deleteRecipe = async (req, res) => {
+// exports.createRecipe = async (req, res) => {
+
+// };
+
+
+// exports.deleteRecipe = async (req, res) => {
    
-};
+// };
 
-exports.updateRecipe = async (req, res) => {
+// exports.updateRecipe = async (req, res) => {
    
-};
+// };
